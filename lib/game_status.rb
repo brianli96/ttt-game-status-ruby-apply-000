60f64c5a4edb2da6e72e06f2b291bcbc1b28c9de
Brian Li <brian.li96@yahoo.com>
2020-09-30 17:16:40 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,55 +4,61 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
- WIN_COMBINATIONS = [ 
-[0,1,2], 
-[3,4,5], 
-[6,7,8], 
-[0,3,6], 
-[1,4,7], 
-[2,5,8], 
-[0,4,8],
-[6,4,2]
+
+
+# Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [6, 4, 2]
 ]
 
 def won?(board)
- WIN_COMBINATIONS.each do |win_combination|
-   win_index_1 = win_combination[0]
-   win_index_2 = win_combination[1]
-   win_index_3 = win_combination[2]
-   position_1 = board[win_index_1] # value of board at win_index_1
-   position_2 = board[win_index_2] # value of board at win_index_2
-   position_3 = board[win_index_3] # value of board at win_index_3
-   if position_1 == "X" && position_2 == "X" && position_3 == "X"
+  WIN_COMBINATIONS.each {|win_combo|
+    index_0 = win_combo[0]
+    index_1 = win_combo[1]
+    index_2 = win_combo[2]
+
+    position_1 = board[index_0]
+    position_2 = board[index_1]
+    position_3 = board[index_2]
+
+    if position_1 == "X" && position_2 == "X" && position_3 == "X"
       return win_combo
     elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
       return win_combo
     end
+  }
   return false
 end
 
 def full?(board)
-   board.all? {|i| i == "X" || i == "O"}
+  board.all? {|index| index == "X" || index == "O"}
 end
 
 def draw?(board)
-   if !won?(board) && full?(board)
-     return true
-   else
-     return false
-   end
+  if !won?(board) && full?(board)
+    return true
+  else
+    return false
+  end
 end
 
 def over?(board)
-   if draw?(board) || won?(board) || full?(board)
-     return true
-   else
-     return false
-   end
+  if won?(board) || full?(board) || draw?(board)
+    return true
+  else
+    return false
+  end
 end
 
-def winner(board)
-   index = []
+def winner (board)
+  index = []
   index = won?(board)
   if index == false
     return nil
