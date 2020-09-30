cad008a3600ddfdb12c54687c2fcc4c997f4ecf8
Brian Li <brian.li96@yahoo.com>
2020-09-30 17:13:53 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -23,8 +23,12 @@ def won?(board)
    position_1 = board[win_index_1] # value of board at win_index_1
    position_2 = board[win_index_2] # value of board at win_index_2
    position_3 = board[win_index_3] # value of board at win_index_3
-   position_1 == position_2 && position_2 == position_3 && position_taken?(board, win_index_1)
- end
+   if position_1 == "X" && position_2 == "X" && position_3 == "X"
+      return win_combo
+    elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
+      return win_combo
+    end
+  return false
 end
 
 def full?(board)
